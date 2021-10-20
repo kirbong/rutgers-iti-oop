2903bb32b96d69bb8e14b53a9532ced1ec26fca8
Kristen <kristen.marcinek@rutgers.edu>
2021-10-20 13:13:18 -0400

    create midterm files

---
+++ Midterm-Project/main.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup # package I am demonstrating
+import requests
+import re
+
+print("Welcome! This program will scrape to find whatever word or phrase you input in my portfolio.")
+print("This program is case-sensitive, so make sure you type correctly!")
+
+def scrape(): # function to be ran for output
+    userInput = input("Type a word or phrase you would like to find: ") # word to be scraped for
+    website = "https://kirbong.github.io/" # my personal portfolio (under construction)
+    data = requests.get(website) # allows beautiful soup to send a request to the website
+    soup = BeautifulSoup(data.text, "html.parser") # parses the page
+    phrase = soup.find_all(string=re.compile('.*{0}.*'.format(userInput))) # scrapes for the word or phrase that the user input
+    
+    if len(phrase) >= 1: # if >= 1, there is a phrase (1 or more instances)
+        print(phrase)
+        print (f"{userInput} was found {len(userInput)} times!")
+    elif len(phrase) == 0: # if there is no phrase, the length would be 0 (no instances)
+        print("Could not find phrase!") 
+    else: # exceptions
+        pass
+
+scrape() # runs scrape function
